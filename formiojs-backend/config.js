8e7b3ffe9923204463c356e5fa882a1b556ce0ef
Rick Warren <rickwarren@selfdesign.org>
2019-05-23 17:08:20 -0700

    initial commit

---
+++ formiojs-backend/config.js
@@ -0,0 +1,5 @@
+const config = {
+    jwt_secret: 'SelfDesign2018'
+}
+
+module.exports = config;
---
+++ formiojs-backend/dist/config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var config = {
+    jwt_secret: 'SelfDesign2018'
+};
+
+module.exports = config;
